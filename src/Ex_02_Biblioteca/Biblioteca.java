aab53fde21b27013e9987fcb61393ff54dcc4ac1
thaisbreder <thais.breder@zup.com.br>
2023-06-21 21:19:37 -0300

    :truck: chore: renomeia classe

--- src/Ex_02_Biblioteca/Biblioteca.java
+++ src/Ex_02_Biblioteca/Biblioteca.java
@@ -1,13 +1,13 @@
 package Ex_02_Biblioteca;
 
-public class Livros {
-    private int isbn;
+public class Biblioteca {
+    private String isbn;
     private String nomeDoLivro;
     private String genero;
     private String autor;
 
     //construtores que recebem os parâmetros de acordo com o enunciado do exercício
-    public Livros(int isbn, String nomeDoLivro, String genero, String autor) {
+    public Biblioteca(String isbn, String nomeDoLivro, String genero, String autor) {
         this.isbn = isbn;
         this.nomeDoLivro = nomeDoLivro;
         this.genero = genero;
@@ -15,24 +15,24 @@ public class Livros {
     }
 
     //métodos "registrarLivro" que recebem os parâmetros e imprimem a mensagem de registro
-    public void registrarLivro(int isbn) {
+    public void registrarLivro(String isbn) {
         System.out.println("O livro foi registrado com o ISBN " + isbn + "." );
     }
 
-    public void registrarLivro(int isbn, String nomeDoLivro) {
+    public void registrarLivro(String isbn, String nomeDoLivro) {
         System.out.println("O livro " + "'" + nomeDoLivro + "'" + " foi registrado com o ISBN " + isbn + "." );
     }
 
-    public void registrarLivro(int isbn, String nomeDoLivro, String genero) {
+    public void registrarLivro(String isbn, String nomeDoLivro, String genero) {
         System.out.println("O livro " + "'" + nomeDoLivro + "'" + " foi registrado com o ISBN " + isbn + " e o gênero " + genero + "." );
     }
 
-    public void registrarLivro(int isbn, String nomeDoLivro, String genero, String autor) {
+    public void registrarLivro(String isbn, String nomeDoLivro, String genero, String autor) {
         System.out.println("O livro " + "'" + nomeDoLivro + "'" +" do autor " + autor + " foi registrado com o ISBN " + isbn + " e o gênero " + genero + ".");
     }
 
-    //metodos getter que retornam as informações referentes ao livro
-    public int getIsbn() {
+    //métodos getter que retornam as informações referentes ao livro
+    public String getIsbn() {
         return isbn;
     }
 
@@ -49,7 +49,7 @@ public class Livros {
     }
 
     public void exibirInformacoes() {
-        System.out.println("Livro: " + nomeDoLivro);
+        System.out.println("Nome: " + nomeDoLivro);
         System.out.println("ISBN: " + isbn);
         System.out.println("Gênero: " + genero);
         System.out.println("Autor: " + autor);
--- src/Ex_02_Biblioteca/Main.java
+++ src/Ex_02_Biblioteca/Main.java
@@ -2,35 +2,35 @@ package Ex_02_Biblioteca;
 
 public class Main {
     public static void main(String[] args) {
-        Livros livro1 = new Livros(1234, "Rita Lee: uma autobiografia", "Autobiografia", "Rita Lee");
-        Livros livro2 = new Livros(5678, "O Menino do Pijama Listrado", "Ficção", null);
-        Livros livro3 = new Livros(9012, "Harry Potter e a Pedra Filosofal", null, null);
-        Livros livro4 = new Livros(3461, null, null, null);
+        Biblioteca livro1 = new Biblioteca("9789896661458", "Rita Lee: uma autobiografia", "Autobiografia", "Rita Lee");
+        Biblioteca livro2 = new Biblioteca("9780857533937", "O Menino do Pijama Listrado", "Ficção", " ");
+        Biblioteca livro3 = new Biblioteca("9788869183157", "Harry Potter e a Pedra Filosofal", " ", " ");
+        Biblioteca livro4 = new Biblioteca("9788869183157",  " ", " ", " ");
 
         System.out.println();
         System.out.println("***************************** REGISTRO DE LIVROS NA BIBLIOTECA *****************************");
         System.out.println();
 
-        System.out.println("| Livro 1 |");
-        System.out.println("--------------");
+        System.out.println("\n| Livro 1 |");
+        System.out.println("------------------------------------------------------------------------------------------------------------------------------");
         livro1.exibirInformacoes();
         livro1.registrarLivro(livro1.getIsbn(), livro1.getNomeDoLivro(), livro1.getGenero(), livro1.getAutor());
         System.out.println();
 
-        System.out.println("| Livro 2 |");
-        System.out.println("--------------");
+        System.out.println("\n| Livro 2 |");
+        System.out.println("------------------------------------------------------------------------------------------------------------------------------");
         livro2.exibirInformacoes();
         livro2.registrarLivro(livro2.getIsbn(), livro2.getNomeDoLivro(), livro2.getGenero());
         System.out.println();
 
-        System.out.println("| Livro 3 |");
-        System.out.println("--------------");
+        System.out.println("\n| Livro 3 |");
+        System.out.println("------------------------------------------------------------------------------------------------------------------------------");
         livro3.exibirInformacoes();
         livro3.registrarLivro(livro3.getIsbn(), livro3.getNomeDoLivro());
         System.out.println();
 
-        System.out.println("| Livro 4 |");
-        System.out.println("--------------");
+        System.out.println("\n| Livro 4 |");
+        System.out.println("------------------------------------------------------------------------------------------------------------------------------");
         livro4.exibirInformacoes();
         livro4.registrarLivro(livro4.getIsbn());
     }
